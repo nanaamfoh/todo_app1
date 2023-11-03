23aad002845e7663947e36e6d8d8931812a7db91
Richie Quainoo <84714883+richmondquainoo@users.noreply.github.com>
2023-11-03 15:22:38 +0000

    Todo first

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_app/screens/home_screen.dart';
+import 'package:todo_app/todo_bloc/todo_bloc.dart';
+import 'package:todo_app/todo_bloc/todo_event.dart';
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getTemporaryDirectory(),
+  );
+  // Bloc.observer = SimpleBlocObserver();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlocProvider<TodoBloc>(
+        create: (context) => TodoBloc()..add(
+            TodoStarted()
+        ),
+        child: const HomeScreen(),
+      ),
+    );
+  }
+}
+
